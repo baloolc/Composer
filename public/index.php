1e7aa21273c5d095690ad4a4d2e4ebfce367a649
baloolc <viann45@hotmail.fr>
2022-04-06 19:28:03 +0200

    insert cow

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require  '../vendor/autoload.php';
 use App\Wcs\Hello;
+use CowSay\Cow as CowSayCow;
 
 $hello = new Hello();
 echo $hello->talk();
+
+$cow = new CowSayCow('Hey, BooBoo!');
+echo $cow;
